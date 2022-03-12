11d0573660dbcc09a15f743b9221d695142cbbd9
Anchidin Valentin Stefan <anchidins0@gmail.com>
2022-03-12 10:55:18 +0100

    Categorie vehicule

--- src/Entity/CategorieVehicule.php
+++ src/Entity/CategorieVehicule.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieVehiculeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieVehiculeRepository::class)]
@@ -15,6 +17,14 @@ class CategorieVehicule
 
     #[ORM\Column(type: 'string', length: 255)]
     private $libelle;
+
+    #[ORM\OneToMany(mappedBy: 'categorieVehicule', targetEntity: TypeVehicule::class)]
+    private $type;
+
+    public function __construct()
+    {
+        $this->type = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class CategorieVehicule
     public function setLibelle(string $libelle): self
     {
         $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|TypeVehicule[]
+     */
+    public function getType(): Collection
+    {
+        return $this->type;
+    }
+
+    public function addType(TypeVehicule $type): self
+    {
+        if (!$this->type->contains($type)) {
+            $this->type[] = $type;
+            $type->setCategorieVehicule($this);
+        }
+
+        return $this;
+    }
+
+    public function removeType(TypeVehicule $type): self
+    {
+        if ($this->type->removeElement($type)) {
+            // set the owning side to null (unless already changed)
+            if ($type->getCategorieVehicule() === $this) {
+                $type->setCategorieVehicule(null);
+            }
+        }
 
         return $this;
     }
